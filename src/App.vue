0126c0989cda1cb89ec19862bd3d13a4d06358c8
Francesco <med.dama@gmail.com>
2020-04-22 08:53:53 +0100

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Tweets</h1>
+    <list-of-tweets-item v-for="(tweet, index) in tweets" :key="index":tweet="tweet" ></list-of-tweets-item>
+    <h2>Total Number of Tweets:{{totalTweets}}</h2>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListOfTweets from './components/ListOfTweets.vue'
 
 export default {
   name: 'App',
+  data() {
+  return {
+    tweets:[
+      {
+        id: 1,
+        name: 'James',
+        handle: '@jokerjames',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: "If you don't succeed, dust yourself off and try again.",
+        likes: 10,
+      },
+      {
+        id: 2,
+        name: 'Fatima',
+        handle: '@fantasticfatima',
+        img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+        tweet: 'Better late than never but never late is better.',
+        likes: 12,
+      },
+      {
+        id: 3,
+        name: 'Xin',
+        handle: '@xeroxin',
+        img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+        tweet: 'Beauty in the struggle, ugliness in the success.',
+        likes: 18,
+      }
+    ]
+  }
+},
   components: {
-    HelloWorld
+    'list-of-tweets-item': ListOfTweets
+
+  },
+  computed: {
+    totalTweets: function() {
+      return this.tweets.reduce((total, tweet) => {
+        return total + tweet.likes;
+      }, 0);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ListOfTweets.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div >
+    <h1>{{ tweet.name }}</h1>
+    <p>{{ tweet.handle }}</p>
+    <p>{{ tweet.img }}</p>
+    <p>{{ tweet.tweet }}</p>
+    <p>{{ tweet.likes }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list-of-tweets-item',
+  props: ['tweet']
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
